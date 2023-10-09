e280b04c9e86e4da7c9f5ebc76779fa72e0181be
Benoit Bovy <benbovy@gmail.com>
2023-10-09 22:56:04 +0200

    Revert "temporarily disable cpp tests"
    This reverts commit 1824fda45dff27be2ddd4aaf09e2e83c2fc50b98.

--- recipe/install_cpp_headers.sh
+++ recipe/install_cpp_headers.sh
@@ -2,18 +2,18 @@
 set -e
 
 # run tests
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-#   cmake -S . \
-#         -B build/tests \
-#         ${CMAKE_ARGS} \
-#         -DFS_BUILD_TESTS=ON \
-#         -DFS_DOWNLOAD_GTEST=ON
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  cmake -S . \
+        -B build/tests \
+        ${CMAKE_ARGS} \
+        -DFS_BUILD_TESTS=ON \
+        -DFS_DOWNLOAD_GTEST=ON
 
-#   cmake --build build/tests -- -j${CPU_COUNT}
-#   cd build/tests
-#   ctest -T test --output-on-failure
-#   cd ../..
-# fi
+  cmake --build build/tests -- -j${CPU_COUNT}
+  cd build/tests
+  ctest -T test --output-on-failure
+  cd ../..
+fi
 
 # install
 cmake -S . \
